1a80c1bd25283077959e58ddc98d25ef09a0c47f
Brian <brianallen5193@gmail.com>
2017-08-10 16:11:06 -0400

    Fix null bug found during testing

--- db/migrate/20170810151613_add_profile_image_to_author.rb
+++ db/migrate/20170810151613_add_profile_image_to_author.rb
@@ -1,5 +1,5 @@
 class AddProfileImageToAuthor < ActiveRecord::Migration[5.1]
   def change
-    add_column :authors, :profile_image, :string, :default => null
+    add_column :authors, :profile_image, :string
   end
 end
